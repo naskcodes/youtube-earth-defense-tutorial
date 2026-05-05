9cfeed0a46cd67b26b3bff02560349c6089cfe7c
ROBERTA NASCIMENTO <ROBERTA.NASCIMENTO@hospitalsantamarta.com.br>
2026-05-05 12:31:40 -0300

    aula três: uma nova forma de se adicionar sprites, imagens e áudio referindo-se ao JSON com os detalhes dos recursos. Extremamente interessante. Uso do for para evitar repetições ao se colocar múltiplos backgrounds.

--- src/scenes/game-scene.js
+++ src/scenes/game-scene.js
@@ -1,7 +1,11 @@
 import Phaser from '../lib/phaser.js';
 import { SCENE_KEYS } from '../common/scene-keys.js';
+import { ASSET_KEYS } from '../common/assets.js';
 
 export class GameScene extends Phaser.Scene {
+    #player
+    #planet
+    
     constructor() {
         super({
             key: SCENE_KEYS.GAME_SCENE,
@@ -9,6 +13,11 @@ export class GameScene extends Phaser.Scene {
     }
 
     create() {
-        console.log('game-scene create called');
+        for (let i = 1; i < 4; i++) {
+            this.add.sprite(0, 0, ASSET_KEYS[`BACKGROUND_${i}`], 0).setOrigin(0).setScale(1, 1.25).play(ASSET_KEYS[`BACKGROUND_${i}`]).setAlpha(0.4);
+        }
+
+       this.#planet = this.add.sprite(this.scale.width/2, this.scale.height/2, ASSET_KEYS.PLANET, 0).play(ASSET_KEYS.PLANET);
+       this.#player = this.add.image(200, 200, ASSET_KEYS.SHIP);
     }
 }
--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -1,5 +1,6 @@
 import Phaser from '../lib/phaser.js';
 import { SCENE_KEYS } from '../common/scene-keys.js';
+import { IMAGE_ASSETS, AUDIO_ASSETS, SPRITESHEET_ASSETS } from '../common/assets.js';
 
 export class PreloadScene extends Phaser.Scene {
     constructor() {
@@ -8,8 +9,33 @@ export class PreloadScene extends Phaser.Scene {
         });
     }
 
+    preload() {
+        IMAGE_ASSETS.forEach((asset) => {
+            this.load.image(asset.assetKey, asset.path);
+        });
+
+        AUDIO_ASSETS.forEach((asset) => {
+            this.load.audio(asset.assetKey, asset.path);
+        });
+
+        SPRITESHEET_ASSETS.forEach((asset) => {
+            this.load.spritesheet(asset.assetKey, asset.path, {
+                frameWidth: asset.frameWidth,
+                frameHeight: asset.frameHeight,
+            });
+        });
+    }
+
     create() {
-        console.log('preload-scene create called');
+        SPRITESHEET_ASSETS.forEach((asset) => {
+            this.anims.create({
+                key: asset.assetKey,
+                frames: this.anims.generateFrameNumbers(asset.assetKey),
+                frameRate: asset.frameRate,
+                repeat: asset.repeat,
+            });
+        });
+
         this.scene.start(SCENE_KEYS.GAME_SCENE);
     }
 }
