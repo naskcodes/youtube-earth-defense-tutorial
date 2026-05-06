aba15119f9b4fba56408a6d61b1941cb01f66cfa
ROBERTA NASCIMENTO <ROBERTA.NASCIMENTO@hospitalsantamarta.com.br>
2026-05-06 15:12:19 -0300

    Adição do radianos de rotação da nave com o planeta no centro

--- src/scenes/game-scene.js
+++ src/scenes/game-scene.js
@@ -3,8 +3,10 @@ import { SCENE_KEYS } from '../common/scene-keys.js';
 import { ASSET_KEYS } from '../common/assets.js';
 
 export class GameScene extends Phaser.Scene {
-    #player
-    #planet
+    #player;
+    #planet;
+    #cursorKeys;
+    #playerAngleInRadians;
     
     constructor() {
         super({
@@ -18,6 +20,27 @@ export class GameScene extends Phaser.Scene {
         }
 
        this.#planet = this.add.sprite(this.scale.width/2, this.scale.height/2, ASSET_KEYS.PLANET, 0).play(ASSET_KEYS.PLANET);
-       this.#player = this.add.image(200, 200, ASSET_KEYS.SHIP);
+       this.#player = this.add.image(0, 0, ASSET_KEYS.SHIP);
+       this.#playerAngleInRadians = 0;
+       this.#updatePlayerPosition();
+
+       this.#cursorKeys = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if (this.#cursorKeys.left.isDown) {
+            this.#playerAngleInRadians -= 0.06;
+        } else if (this.#cursorKeys.right.isDown) {
+            this.#playerAngleInRadians +=0.06
+        }
+
+        this.#updatePlayerPosition();
+    }
+
+    #updatePlayerPosition() {
+        const x = this.scale.width / 2 + (this.#planet.displayHeight / 2) * Math.cos(this.#playerAngleInRadians);
+        const y = this.scale.height / 2 + (this.#planet.displayHeight / 2) * Math.sin(this.#playerAngleInRadians);
+        this.#player.setPosition(x, y);
+        this.#player.rotation = this.#playerAngleInRadians + Math.PI / 2;
     }
 }
